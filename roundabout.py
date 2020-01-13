c71ceb6d51933e426b5d910f5729c32be70f5511
Dorian <dorian4840@gmail.com>
2020-01-13 13:47:56 +0100

    Improved Roundabout classes

--- roundabout.py
+++ roundabout.py
@@ -3,22 +3,26 @@ import numpy as np
 class Roundabout():
     def __init__(self):
         self.exceptions = []
-        self.area = []
         self.queue = np.array(([]))
     
     # Gets all the indexes between 2 points in the matrix.
-    def get_area(self, area, x, y):
-        x1, y1 = x
-        x2, y2 = y
-        for i in range(x1, x2+1):
-            for j in range(y1, y2+1):
-                area.append((i,j))
+    def get_area(self, points):
+        area = []
+
+        # Each element in points is a tuple of 2 coordinates. Each coordinate is a tuple of its x and y value.s
+        for point in points:
+            x1, y1 = point[0]
+            x2, y2 = point[1]
+            for i in range(x1, x2+1):
+                for j in range(y1, y2+1):
+                    area.append((i,j))
         return area
 
 
 class Regular(Roundabout):
     def __init__(self, name):
         self.size = 11
+        self.points = [((2,2),(8,8))]
         self.grid = np.array([[0, 0, 1, 1, 0, 0, 0, 2, 2, 0, 0],
                               [0, 0, 5, 5, 0, 0, 0, 5, 5, 0, 0],
                               [2, 5, 6, 5, 5, 5, 5, 5, 6, 5, 1],
@@ -30,7 +34,7 @@ class Regular(Roundabout):
                               [1, 5, 6, 5, 5, 5, 5, 5, 6, 5, 2],
                               [0, 0, 5, 5, 0, 0, 0, 5, 5, 0, 0],
                               [0, 0, 2, 2, 0, 0, 0, 1, 1, 0, 0]])
-       area = self.get_area()
+        self.area = self.get_area(self.points)
 
 
 class Turbo(Roundabout):
@@ -84,4 +88,4 @@ class Magic(Roundabout):
 
 
 round = Regular('regular')
-print(round.grid)
+print(round.area)
