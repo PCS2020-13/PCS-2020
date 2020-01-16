13aaf0b8d8ad6ac5231e4ae206fa2c52674ff299
Dorian <dorian.bekaert@student.uva.nl>
2020-01-16 16:03:42 +0100

    Minder opstoppingen, wait_ctr fixed

--- regular_turbo.py
+++ regular_turbo.py
@@ -101,112 +101,114 @@ def process_cars_reg(self):
         drive_outside(self, car)
 
 def drive_roundabout(self, car, wait_ctr=2):
-        """Let a car drive on the roundabout.
+    """Let a car drive on the roundabout.
 
-        Arguments:
-            car {Car} -- The car that should drive.
-        """
-        row, col = car.cur_pos
-        state = self.model.grid[row][col]
+    Arguments:
+        car {Car} -- The car that should drive.
+    """
+    row, col = car.cur_pos
+    state = self.model.grid[row][col]
 
-        # State 8 defines the exceptions
-        if state == 8:
-            state = exception_handling(self, car)
+    # State 8 defines the exceptions
+    if state == 8:
+        state = exception_handling(self, car)
 
-        if state == 3:
-            if self.priority(car, car.look_left()):
-                car.turn_left()
-                car.drive()
-        elif state == 5:
-            if self.priority(car, car.orientation):
-                car.drive()
-        elif state == 6:
-            car.turn_ctr += 1
-            prob = car.turn_ctr * (1/4)
-            if prob > 1:
-                prob = 1
+    if state == 3:
+        if self.priority(car, car.look_left()):
+            car.turn_left()
+            car.drive()
+    elif state == 5:
+        if self.priority(car, car.orientation):
+            car.drive()
+    elif state == 6:
+        car.turn_ctr += 1
+        prob = car.turn_ctr * (1/4)
+        if prob > 1:
+            prob = 1
 
-            # Checks if car stands still for more than 3 turns
-            if car.prev_pos[1] >= wait_ctr:
-                prob = 1-prob
+        # Checks if car stands still for more than 3 turns
+        if car.prev_pos[1] == wait_ctr:
+            prob = 1-prob
+            car.prev_pos[1] = 0
 
-            turn = RandomState().binomial(1, p=prob)
-            if turn == 1:
-                if self.priority(car, car.look_right()):
-                    car.turn_right()
-                    car.drive()
-                else:
-                    car.turn_ctr = 3
-            else:
-                if self.priority(car, car.orientation):
-                    car.drive()
-        elif state == 7:
+        turn = RandomState().binomial(1, p=prob)
+        if turn == 1:
             if self.priority(car, car.look_right()):
                 car.turn_right()
                 car.drive()
-        elif state == 9:
-            if car.switch_ctr == 0 and self.priority(car, car.look_right()):
-                if RandomState().binomial(1, p=1/2) == 1:
-                    car.turn_right()
-                    car.drive()
-                    car.turn_left()
-                    if self.priority(car, car.orientation):
-                        car.drive()
-                        car.switch_ctr = 1
-                    else:
-                        car.turn_left()
-                        car.drive()
-                        car.turn_right()
-                        if self.priority(car, car.orientation):
-                            car.drive()
-                elif self.priority(car, car.orientation):
-                    car.drive()
-            elif self.priority(car, car.orientation):
-                car.drive()
-
-        elif state == 10:
-            if car.switch_ctr == 0 and self.priority(car, car.look_left()):
-                if RandomState().binomial(1, p=1/2) == 1:
-                    car.turn_left()
-                    car.drive()
-                    car.turn_right()
-                    if self.priority(car, car.orientation):
-                        car.drive()
-                        car.switch_ctr = 1
-                    else:
-                        car.turn_right()
-                        car.drive()
-                        car.turn_left()
-                        if self.priority(car, car.orientation):
-                            car.drive()
-                elif self.priority(car, car.orientation):
-                    car.drive()
-            elif self.priority(car, car.orientation):
-                car.drive()
-
-        if np.array_equal(car.cur_pos, car.prev_pos[0]):
-            car.prev_pos[1] += 1
+            else:
+                car.turn_ctr = 3
         else:
-            car.prev_pos[1] = 0
-        car.prev_pos[0] = car.cur_pos
+            if self.priority(car, car.orientation):
+                car.drive()
+    elif state == 7:
+        if self.priority(car, car.look_right()):
+            car.turn_right()
+            car.drive()
+    elif state == 9:
+        if car.switch_ctr == 0 and self.priority(car, car.look_right()):
+            if RandomState().binomial(1, p=1/2) == 1:
+                car.turn_right()
+                car.drive()
+                car.turn_left()
+                if self.priority(car, car.orientation):
+                    car.drive()
+                    car.switch_ctr = 1
+                else:
+                    car.turn_left()
+                    car.drive()
+                    car.turn_right()
+                    if self.priority(car, car.orientation):
+                        car.drive()
+            elif self.priority(car, car.orientation):
+                car.drive()
+        elif self.priority(car, car.orientation):
+            car.drive()
+
+    elif state == 10:
+        if car.switch_ctr == 0 and self.priority(car, car.look_left()):
+            if RandomState().binomial(1, p=1/2) == 1:
+                car.turn_left()
+                car.drive()
+                car.turn_right()
+                if self.priority(car, car.orientation):
+                    car.drive()
+                    car.switch_ctr = 1
+                else:
+                    car.turn_right()
+                    car.drive()
+                    car.turn_left()
+                    if self.priority(car, car.orientation):
+                        car.drive()
+            elif self.priority(car, car.orientation):
+                car.drive()
+        elif self.priority(car, car.orientation):
+            car.drive()
+
+    if np.array_equal(car.cur_pos, car.prev_pos[0]):
+        car.prev_pos[1] += 1
+
+    car.prev_pos[0] = car.cur_pos
+
+
 
 def drive_outside(self, car):
-        """Let a car drive outside of the roundabout.
+    """Let a car drive outside of the roundabout.
 
-        Arguments:
-            car {Car} -- The car that should drive.
-        """
-        row, col = car.cur_pos
-        state = self.model.grid[row][col]
+    Arguments:
+        car {Car} -- The car that should drive.
+    """
+    row, col = car.cur_pos
+    state = self.model.grid[row][col]
 
-        # Check if nothing is in front of the car
-        if self.priority(car, car.orientation):
-            if state == 1:
-                self.free_starts = np.append(
-                    self.free_starts, [car.cur_pos], axis=0)
-            elif state == 2:
-                car.toggle_active()
-                self.n_finished += 1
-                self.turns_per_car.append(car.turns)
-                return
-            car.drive()
+    # Check if nothing is in front of the car
+    if self.priority(car, car.orientation):
+        if state == 1:
+            self.free_starts = np.append(
+                self.free_starts, [car.cur_pos], axis=0)
+        elif state == 2:
+            car.toggle_active()
+            self.n_finished += 1
+            self.turns_per_car.append(car.turns)
+            return
+        car.drive()
