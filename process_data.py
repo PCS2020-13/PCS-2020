66efbe4c92058af916b7805b2e3109911e091051
Florine de Geus <florine.degeus@student.uva.nl>
2020-01-22 16:30:50 +0100

    Added confidence interval

--- process_data.py
+++ process_data.py
@@ -2,6 +2,8 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm
+import scipy.stats as spstats
 
 import fnmatch
 import os
@@ -9,44 +11,76 @@ import os
 ROUNDABOUTS = ['regular', 'turbo', 'magic']
 DENSITY = np.arange(0.5, 10, 0.5)/10
 STEPS = 1000
-ASSHOLE_PROB = [0, 0.05, 0.1, 0.25]
+ASSHOLE_PROB = [0, 0.05, 0.1]
 
-def read_data(ROUNDABOUTS, DENSITY, STEPS, ASSHOLE_PROB):
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+plt.style.use('seaborn-poster')
+
+
+def read_data():
     r_data = []
     for r in ROUNDABOUTS:
         a_data = []
+
         for a in ASSHOLE_PROB:
             d_data = []
+
             for d in DENSITY:
                 filename = 'output/{}_{}_{}_{}.csv'.format(r, d, STEPS, a)
-                d_data.append(np.loadtxt(filename, skiprows=1, 
-                                         usecols=range(1,5), delimiter=','))
+                d_data.append(np.loadtxt(filename, skiprows=1,
+                                         usecols=range(1, 5), delimiter=','))
+
             a_data.append(d_data)
+
         r_data.append(a_data)
+
     return r_data
 
-def make_graph(data, ROUNDABOUTS, DENSITY, STEPS, ASSHOLE_PROB, error=False):
-    for i,r in enumerate(ROUNDABOUTS):
+
+def make_graph(data, error=False, save=False):
+    for i, r in enumerate(ROUNDABOUTS):
         fig = plt.figure()
-        for j,a in enumerate(ASSHOLE_PROB):
+
+        for j, _ in enumerate(ASSHOLE_PROB):
             avg_throughput = []
-            std_throughput = []
+            err_throughput = []
+
             for d in data[i][j]:
-                throughput = d[:,1]/STEPS
+                throughput = d[:, 1]/STEPS
                 avg_throughput.append(np.mean(throughput))
-                std_throughput.append(np.std(throughput))
+                err_throughput.append(mean_confidence_interval(throughput))
+
+            avg_throughput = np.array(avg_throughput)
+            err_throughput = np.array(err_throughput)
 
             if error:
-                plt.errorbar(DENSITY, avg_throughput, yerr=std_throughput)
-            else:
-                plt.plot(DENSITY, avg_throughput)
-        plt.ylim(0,3)
-        plt.title(r)
+                plt.fill_between(DENSITY, avg_throughput - err_throughput,
+                                 avg_throughput + err_throughput, alpha=0.2)
+
+            plt.plot(DENSITY, avg_throughput)
+
+        plt.ylim(0, 3)
+        plt.title('{} roundabout'.format(r.capitalize()))
         plt.xlabel('Density')
         plt.ylabel('Average throughput')
         plt.legend(ASSHOLE_PROB, title='Asshole Probability', loc=4)
 
+        if save:
+            fig.savefig('figures/{}.pdf'.format(r),
+                        transparant=True, bbox_inches='tight')
+            fig.savefig('figures/{}.png'.format(r),
+                        transparant=True, bbox_inches='tight')
+
+
+def mean_confidence_interval(data, confidence=0.95):
+    a = 1.0 * np.array(data)
+    n = len(a)
+    se = spstats.sem(a)
+    return se * spstats.t.ppf((1 + confidence) / 2., n - 1)
+
+
 if __name__ == '__main__':
-    data = read_data(ROUNDABOUTS, DENSITY, STEPS, ASSHOLE_PROB)
-    make_graph(data, ROUNDABOUTS, DENSITY, STEPS, ASSHOLE_PROB)
+    data = read_data()
+    make_graph(data, error=True, save=True)
     plt.show()
