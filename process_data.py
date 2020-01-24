8562b9a5c3ee0df59e6e8670f5132fb9860cb0a3
Dorian <dorian.bekaert@student.uva.nl>
2020-01-24 14:00:05 +0100

    added ttest

--- process_data.py
+++ process_data.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import scipy.stats as spstats
+import itertools as ittool
 
 import fnmatch
 import os
@@ -17,7 +18,7 @@ plt.rc('font', family='serif')
 plt.style.use('seaborn-poster')
 
 
-def read_data():
+def read_data(test=True):
     r_data = []
     for r in ROUNDABOUTS:
         a_data = []
@@ -31,8 +32,10 @@ def read_data():
                                          usecols=range(1, 5), delimiter=','))
 
             a_data.append(d_data)
-
         r_data.append(a_data)
+
+    if test:
+        ttest(r_data)
 
     return r_data
 
@@ -79,7 +82,45 @@ def mean_confidence_interval(data, confidence=0.95):
     return se * spstats.t.ppf((1 + confidence) / 2., n - 1)
 
 
+def ttest(data, test_ass=False, test_round=False):
+    if test_ass:
+        for i in range(len(ROUNDABOUTS)):
+            print('=========================')
+            print('========= {} ========='.format(ROUNDABOUTS[i]))
+            data_reg = []
+            for asshole in data[i]:
+                data_ass = []
+                for d in asshole:
+                    throughput = d[:, 1]/STEPS
+                    data_ass.append(np.mean(throughput))
+                data_reg.append(data_ass)
+
+            iters = list(ittool.combinations(range(6), 2))
+            for i in iters:
+                print('== {} vs {} =='.format(ASSHOLE_PROB[i[0]], ASSHOLE_PROB[i[1]]))
+                t, p = spstats.ttest_ind(data_reg[i[0]], data_reg[i[1]])
+                print('t-stat: {}'.format(abs(round(t, 5))))
+                print('p-value: {}\n'.format(round(p, 5)))
+    
+    if test_round:
+        iters = list(ittool.combinations(range(3), 2))
+        for i in iters:
+            print('== {} vs {} =='.format(ROUNDABOUTS[i[0]], ROUNDABOUTS[i[1]]))
+            data_a = []
+            for d in data[i[0]][0]:
+                throughput = d[:, 1]/STEPS
+                data_a.append(np.mean(throughput))
+            
+            data_b = []
+            for d in data[i[1]][0]:
+                throughput = d[:, 1]/STEPS
+                data_b.append(np.mean(throughput))
+            
+            t, p = spstats.ttest_ind(data_a, data_b)
+            print('t-stat: {}'.format(abs(round(t, 5))))
+            print('p-value: {}\n'.format(round(p, 5)))
+
 if __name__ == '__main__':
     data = read_data()
-    make_graph(data, error=True, save=True)
+    make_graph(data, error=True, save=False)
     plt.show()
